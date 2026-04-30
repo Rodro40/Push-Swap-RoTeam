7514cdd68bf180b70548f2d4aaefd084736332ce
Rodrigo Alejandro Aguilar Mejia <rodrigoa@student.42madrid.com>
2026-04-30 21:21:03 +0200

    Update strategy.c - NORM

--- src/strategy.c
+++ src/strategy.c
@@ -9,61 +9,63 @@
 /*   Updated: 2026/04/27 19:28:53 by roandres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "push_swap.h"
 
-static void	run_adaptive(t_stack_node **a, t_stack_node **b, t_ctx *ctx)
+static void    set_used_strategy(t_ctx *ctx, t_strategy strat)
 {
-	float	disorder;
-
-	disorder = compute_disorder(*a);
-	if (disorder < 0.2f)
-	{
-		if (ctx->bench)
-			ctx->b.used_strategy = LINEAR;
- 		if (!linear_sort(a))
-    	{
-        	if (ctx->bench)
-            	ctx->b.used_strategy = MEDIUM;
-        	medium_sort(a, b);
-    	}
-		linear_sort(a);
-	}
-	else if (disorder < 0.5f)
-	{
-		if (ctx->bench)
-			ctx->b.used_strategy = MEDIUM;
-		medium_sort(a, b);
-	}
-	else
-	{
-		if (ctx->bench)
-			ctx->b.used_strategy = COMPLEX;
-		complex_sort(a, b);
-	}
-	(void)ctx;
+    if (ctx && ctx->bench)
+        ctx->b.used_strategy = strat;
 }
 
-void	run_strategy(t_stack_node **a, t_stack_node **b, t_ctx *ctx)
+static void    run_low_disorder(t_stack_node **a, t_stack_node **b, t_ctx *ctx)
 {
-	if (ctx->strategy == SIMPLE)
-	{
-		if (ctx->bench)
-            ctx->b.used_strategy = SIMPLE;
-		simple_sort(a, b);
-	}
-	else if (ctx->strategy == MEDIUM)
-	{
-		if (ctx->bench)
-            ctx->b.used_strategy = MEDIUM;
-		medium_sort(a, b);
-	}
-	else if (ctx->strategy == COMPLEX)
-	{
-		if (ctx->bench)
-            ctx->b.used_strategy = COMPLEX;
-		complex_sort(a, b);
-	}
-	else
-		run_adaptive(a, b, ctx);
+    int    ok;
+
+    set_used_strategy(ctx, LINEAR);
+    ok = linear_sort(a);
+    if (!ok)
+    {
+        set_used_strategy(ctx, MEDIUM);
+        medium_sort(a, b);
+    }
+}
+
+static void    run_adaptive(t_stack_node **a, t_stack_node **b, t_ctx *ctx)
+{
+    float    disorder;
+
+    disorder = compute_disorder(*a);
+    if (disorder < 0.2f)
+        run_low_disorder(a, b, ctx);
+    else if (disorder < 0.5f)
+    {
+        set_used_strategy(ctx, MEDIUM);
+        medium_sort(a, b);
+    }
+    else
+    {
+        set_used_strategy(ctx, COMPLEX);
+        complex_sort(a, b);
+    }
+}
+
+void    run_strategy(t_stack_node **a, t_stack_node **b, t_ctx *ctx)
+{
+    if (ctx->strategy == SIMPLE)
+    {
+        set_used_strategy(ctx, SIMPLE);
+        simple_sort(a, b);
+    }
+    else if (ctx->strategy == MEDIUM)
+    {
+        set_used_strategy(ctx, MEDIUM);
+        medium_sort(a, b);
+    }
+    else if (ctx->strategy == COMPLEX)
+    {
+        set_used_strategy(ctx, COMPLEX);
+        complex_sort(a, b);
+    }
+    else
+        run_adaptive(a, b, ctx);
 }
